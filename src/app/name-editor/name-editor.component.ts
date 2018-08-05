9503262073e0ea229db05088d5ac8552fc8c7ea7
AdamLasak <lasak.ad@gmail.com>
2018-08-05 12:17:02 +0200

    validation completed

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { 
+	ValidationErrors,
+	AbstractControl, 
+	ValidatorFn, 
+	FormArray, 
+	Validators, 
+	FormBuilder, 
+	FormGroup, 
+	FormControl 
+} from '@angular/forms';
 
 @Component({
 	selector: 'app-name-editor',
@@ -8,12 +17,18 @@ import { FormArray, Validators, FormBuilder, FormGroup, FormControl } from '@ang
 })
 export class NameEditorComponent implements OnInit {
 
+	constructor(private fb: FormBuilder) { }
+
+	ngOnInit() {
+	}
+
 	public name = new FormControl('');
 
 	public profileForm = new FormGroup({
 		firstName: new FormControl('', [
 			Validators.required,
-			Validators.minLength(4)
+			Validators.minLength(4),
+			forbiddenNameValidator(/bob/i)
 			]),
 		lastName: new FormControl(''),
 		address: new FormGroup({
@@ -22,7 +37,7 @@ export class NameEditorComponent implements OnInit {
       		state: new FormControl(''),
       		zip: new FormControl('')
     		})
-	});
+	}, {validators: firstNameNotEqualLastName});
 
 	public profileFormBuilder = this.fb.group({
 		firstName: ['', Validators.required],
@@ -39,14 +54,13 @@ export class NameEditorComponent implements OnInit {
 			])
 	});
 
-	constructor(private fb: FormBuilder) { }
-
-	ngOnInit() {
-	}
-
 	public get firstName() {
 		return this.profileForm.get('firstName');
 	}
+
+	public get lastName() {
+		return this.profileForm.get('lastName');
+	}	
 
 	public get aliases() {
 		return this.profileFormBuilder.get('aliases') as FormArray;
@@ -91,5 +105,23 @@ export class NameEditorComponent implements OnInit {
 
 // if name contains regex expresion
 export function forbiddenNameValidator(name: RegExp): ValidatorFn {
-	return (control: AbstractControl): {/* f-ction*/};
+	return (control: AbstractControl): {[key: string]: any} | null => {
+		const containsRegex = name.test(control.value);
+		if (containsRegex) {
+			return {'forbiddenName': {value: control.value}};
+		} else {
+			return null;
+		}
+	};
 }
+
+export const firstNameNotEqualLastName: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+	const first = control.get('firstName');
+	const last = control.get('lastName');
+	
+	if (first.value === last.value) {
+		return {'namesAreEqual': true};
+	} else {
+		return null;
+	}
+};
