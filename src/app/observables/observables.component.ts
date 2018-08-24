dc94a388486b9cb79a682ab6c7f565f7f8c879cb
AdamLasak <lasak.ad@gmail.com>
2018-08-24 17:57:45 +0200

    RxJS

--- src/app/observables/observables.component.ts
+++ src/app/observables/observables.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, interval, fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { retry, catchError, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observables',
@@ -9,7 +11,6 @@ import { Observable, of } from 'rxjs';
 export class ObservablesComponent implements OnInit {
 
 	public observableMessages: Array<string> = [];
-	public customObservableMessages: Array<string> = [];
 
 	public myObservable = of(1, 2, 3, 4, 5);
 	public myObserver = {
@@ -17,6 +18,10 @@ export class ObservablesComponent implements OnInit {
 		error: err => this.observableMessages.push(`err: ${err}`),
 		complete: () => this.observableMessages.push('observer is complete')
 	};
+
+	public ESC_KEY: number = 27;
+
+	public mouseX: number = 0, mouseY: number = 0;
 
 	constructor() { }
 
@@ -31,10 +36,63 @@ export class ObservablesComponent implements OnInit {
 			next(x) { console.log(x) },
 			complete() { console.log('complete sequence') }
 		});
+
+		const inputBox = document.getElementById('name') as HTMLInputElement;
+		const subscription = fromEvent_custom(inputBox, 'keydown').subscribe(
+			(e: KeyboardEvent) => {
+				if (e.keyCode === this.ESC_KEY) {
+					this.observableMessages.push(`'escape' key pressed`);
+					console.log(`'escape' key pressed`);
+				}
+			}
+		);
+
+		const myInterval = interval(5000);
+		myInterval.subscribe(
+			x => console.warn(`after ${x} iteration has been called this observable`)
+		);
+
+		const mouseBox = document.getElementById('mouse-box') as HTMLInputElement;
+		const mouseMoves = fromEvent(mouseBox, 'mousemove');
+
+		const mouseMovesObservable = mouseMoves.subscribe(
+			(e: MouseEvent) => {
+				this.mouseX = e.clientX;
+				this.mouseY = e.clientY;
+			}
+		);
+
+		const apiData$ = ajax('api/data') // $ means, that this is an Observable variable
+		.pipe(
+			retry(3), // do not use for user authentication
+			map(res => {
+				if (!res.response){
+					throw new Error('Some error');
+				}
+				return res.response;	
+			}),
+			catchError(err => of([]))
+		).subscribe({
+			next(res) { console.log('data: ', res); },
+			error(err) { console.log('errors already caught... will not run'); }
+		});
+
+		const numbers = of(1, 5, 7);
+		const squareNumbers = map(val => val * val);
+		const squaredNumbers = squareNumbers(numbers);
+		squaredNumbers.subscribe(x => console.log(`squared number: ${x}`));
+
+		numbers.pipe(
+			filter(x => x > 1),
+			map(x => x + 1)
+		).subscribe(x => console.log(`pipes nubmers: ${x}`));
 	}
 
 }
 
+
+// CUSTOM OBSERVABLE
+// equivalent of Observable.of(...)
 function customObservable(observer: any) {
 	observer.next(20);
 	observer.next(30);
@@ -43,4 +101,19 @@ function customObservable(observer: any) {
 
 	// nothing more to do -> unsubscribe
 	return {unsubscribe() {}};
+}
+
+// CUSTOM OBSERVABLE
+// equivalent of Observable.of(...)
+function fromEvent_custom(target: any, eventName: string) {
+	return new Observable((observer) => {
+
+		const handler = x => observer.next(x);
+		target.addEventListener(eventName, handler);
+
+		return () => {
+			target.removeEventListener(eventName, handler);
+		};
+
+	});
 }
